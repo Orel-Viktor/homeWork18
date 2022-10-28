be4535b6985dff7bfcab06eec41b02a69c800061
Orel-Viktor <orelviktor93@gamil.com>
2022-10-28 21:17:02 +0300

    3th comit

--- main.js
+++ main.js
@@ -1,34 +1,25 @@
-import './style.css'
-import $ from 'jquery'
+import "./style.css";
+import $ from "jquery";
+
+$(function () {
+  const slideItem = $(".js--gal_item");
+  const nextSlideBtn = $(".js--modal__next");
+  const modalContentImg = $(".js--modal__content img");
+
+  const arrayAttr = [];
+  slideItem.each(function () {
+    arrayAttr.push($(this).find("img").attr("src"));
+  });
+  console.log(arrayAttr[2])
+
+  slideItem.on("click", function () {
+    const attr = $(this).find("img").addClass('current-slide').attr("src");
+    $(".js--modal").removeClass("modal").addClass("modal-active");
+    modalContentImg.attr("src", attr);
 
 
-
-// document.querySelector('.js-gal_item').addEventListener('click',function(){
-//   console.log(2)
-// })
-
-$(function(){
-  const slideItem =  $('.js--gal_item')
-  const nextSlideBtn =$('.js--modal__next')
-  
- 
-
-  const modalContentImg = $('.js--modal__content img' )
-  slideItem.on('click',function(){
-    const attr = $(this).find('img').attr('src')
-    $('.js--modal').removeClass('modal').addClass('modal-active')
-    modalContentImg.attr('src' , attr)
-   
-
-
-    nextSlideBtn.on('click', function(){
-   
-     
-    })
-  })
-
-  
- 
-
-  
-})
+    nextSlideBtn.on("click", function () {
+         
+    });
+  });
+});
