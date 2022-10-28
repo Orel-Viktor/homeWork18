060776b5460c78fd0997c442a8411a74a4e5e6d3
Orel-Viktor <OrelViktor93@gmail.com>
2022-10-28 16:59:20 +0300

    second-commit

--- main.js
+++ main.js
@@ -1,6 +1,34 @@
 import './style.css'
+import $ from 'jquery'
 
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+
+
+// document.querySelector('.js-gal_item').addEventListener('click',function(){
+//   console.log(2)
+// })
+
+$(function(){
+  const slideItem =  $('.js--gal_item')
+  const nextSlideBtn =$('.js--modal__next')
+  
+ 
+
+  const modalContentImg = $('.js--modal__content img' )
+  slideItem.on('click',function(){
+    const attr = $(this).find('img').attr('src')
+    $('.js--modal').removeClass('modal').addClass('modal-active')
+    modalContentImg.attr('src' , attr)
+   
+
+
+    nextSlideBtn.on('click', function(){
+   
+     
+    })
+  })
+
+  
+ 
+
+  
+})
