be99088da18d6903b3245e45c2c43f774ec11d4d
Orel-Viktor <OrelViktor93@gmail.com>
2022-10-29 13:23:44 +0300

    click-modal

--- main.js
+++ main.js
@@ -1,34 +1,71 @@
+'use sctrict'
+
 import './style.css'
 import $ from 'jquery'
 
 
 
-// document.querySelector('.js-gal_item').addEventListener('click',function(){
-//   console.log(2)
-// })
 
-$(function(){
-  const slideItem =  $('.js--gal_item')
-  const nextSlideBtn =$('.js--modal__next')
-  
- 
+$(function () {
+  const gallery = $('.js--gallery')
+  const slideItem = $('.js--gal_item')
+  const nextSlideBtn = $('.js--modal__next')
+  const modalContent = $('.js--modal__content')
 
-  const modalContentImg = $('.js--modal__content img' )
-  slideItem.on('click',function(){
-    const attr = $(this).find('img').attr('src')
+
+  slideItem.on('click', function (event) {
+    $(this).addClass('current')
+    let currentSlide = event.target
     $('.js--modal').removeClass('modal').addClass('modal-active')
-    modalContentImg.attr('src' , attr)
-   
-
-
-    nextSlideBtn.on('click', function(){
-   
-     
-    })
+    const currentModalSlide = currentSlide.cloneNode()
+    modalContent.append(currentModalSlide)
   })
 
-  
- 
 
-  
+  nextSlideBtn.on('click', function () {
+    slideItem.closest('.current').next()
+    console.log(slideItem)
+  })
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const attr = $(this).find('img').attr('src')
+
+// $('.js--modal').removeClass('modal').addClass('modal-active')
+// // modalContentImg.attr('src' , attr)
+
+
+
+
+// nextSlideBtn.on('click', function(){
+// modalContentImg('src', )
+// })
