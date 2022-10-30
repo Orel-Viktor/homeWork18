6a4b137b826ce8b40f72c4e37903c2acdd743f1b
Orel-Viktor <orelviktor93@gamil.com>
2022-10-30 15:50:42 +0300

    homewrk18-redy

--- main.js
+++ main.js
@@ -8,48 +8,52 @@ $(function () {
   const nextSlideBtn = $(".js--modal__next");
   const prevSlideBtn = $(".js--modal__prev");
   const modalContent = $(".js--modal__content");
-  const closeModalBtn = $('.js--modal__close');
+  const closeModalBtn = $(".js--modal__close");
 
-  $('.js--modal').on("click", function (event) {
+  $(".js--modal").on("click", function (event) {
     if (this === event.target) {
-      closeModal()
+      closeModal();
     }
-  })
-  
+  });
+
   slideItems.on("click", function () {
     $(".js--modal").addClass("active");
-    showNewSlide($(this))
+    showNewSlide($(this));
   });
 
   nextSlideBtn.on("click", function () {
     const currentItem = clearCurrentSlide();
-    showNewSlide(currentItem.next().length ? currentItem.next() : $(slideItems[0]))
+    showNewSlide(
+      currentItem.next().length ? currentItem.next() : $(slideItems[0])
+    );
   });
 
   prevSlideBtn.on("click", function () {
     const currentItem = clearCurrentSlide();
-    showNewSlide(currentItem.prev().length ? currentItem.prev() : $(slideItems[slideItems.length - 1]))
-  })
+    showNewSlide(
+      currentItem.prev().length
+        ? currentItem.prev()
+        : $(slideItems[slideItems.length - 1])
+    );
+  });
 
   const clearCurrentSlide = () => {
     modalContent.empty();
     const currentItem = $(".current");
     currentItem.removeClass("current");
     return currentItem;
-  }
+  };
 
   const showNewSlide = (item) => {
     item.addClass("current");
-    const nextImage = item.find('img').clone();
-    modalContent.append(nextImage);
-  }
-  
-  closeModalBtn.on('click', closeModal)
+    const nextImage = item.find("img").clone();
+    modalContent.append(nextImage).css("display", "none").fadeIn("fast");
+  };
 
-  function closeModal () {
-    $(".js--modal").removeClass('active')
-    clearCurrentSlide()
-  }
+  closeModalBtn.on("click", closeModal);
 
+  function closeModal() {
+    $(".js--modal").removeClass("active");
+    clearCurrentSlide();
+  }
 });
-
